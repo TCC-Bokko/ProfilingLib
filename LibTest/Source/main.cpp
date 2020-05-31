359f67bcc9626f32e224e9b97c63387559f297ca
Antonio Cardona Costa <antocard@ucm.es>
2020-05-31 12:07:43 +0200

    Cambios

--- LibTest/Source/main.cpp
+++ LibTest/Source/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
-
+#include "../Profiler/Include/Profiler.h"
 
 void main() {
 	
 	std::cout << "Hello World.\n";
+	testMSG();
 
 	system("PAUSE");
 }
--- Profiler/Source/Profiler.cpp
+++ Profiler/Source/Profiler.cpp
@@ -1,6 +1,8 @@
 // Profiler.cpp : Define las funciones exportadas de la aplicación DLL.
 //
-
+#include <iostream>
 #include "stdafx.h"
 
-
+void testMSG() {
+	std::cout << "Esto funciona.\n";
+}
